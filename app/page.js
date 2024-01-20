82261d9e52bb4009806ac9b44e18b81d9757806f
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-20 14:39:20 +0100

    Updated the page.js file

--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
-export default function Home() {
-	return <div className="uppercase">Hello World!</div>;
+import Center from "@/src/components/Center";
+import Header from "@/src/components/Header";
+
+function Home() {
+	return (
+		<div>
+			{/* Header Section */}
+			<Header />
+
+			{/* Center Section */}
+			<Center />
+		</div>
+	);
 }
+export default Home;
