fa1915d4c594606e57d21d0d0536e72776936b90
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-21 15:53:53 +0100

    Modified main.jsx component

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Center from "./components/Center";
+
+function Main() {
+	return (
+		<div>
+			{/* Header Section */}
+			<Header />
+
+			{/* Center Section */}
+			<Center />
+		</div>
+	);
+}
+
+export default Main;
